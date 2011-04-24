cf16c9c0eb33dc09faec35af31a4991693b60ed4
justan <ckr189@163.com>
2011-04-24 22:31:15 +0800

    session

--- models.js
+++ models.js
@@ -1,15 +1,29 @@
 var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+  Schema = mongoose.Schema,
+  crypto = require('crypto');
 
 var Document = new Schema({
   title: {type: String, index: true},
   data: {type: String},
   tags: {type: String},
   user_id: {type: Number, index: true},
+}),
+User = new Schema({
+  email: {type: String, index: true, unique: true},
+  salt: {type: String, default: function(){
+    return Math.round((new Date().valueOf() * Math.random())) + '';
+  }}
 });
-  
+User.virtual('password').set(function(password){
+  this.set('_password', crypto.createHmac('sha1', this.salt).update(password).digest('hex'));
+});
+
 mongoose.model('Document', Document);
+mongoose.model('User', User);
 
 exports.Document = function(db){
   return db.model('Document');
+};
+exports.User = function(db){
+  return db.model('User');
 };
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,27 +1,29 @@
 $('.destroy').live('click', function(e) {
   e.preventDefault();
   if (confirm('Are you sure you want to delete that item?')) {
-    var element = $(this),
-        form = $('<form></form>');
-    form
-      .attr({
-        method: 'POST',
-        action: '/documents/' + element.val()
-      })
-      .hide()
-      .append('<input type="hidden" name="_method" value="delete"/>');
-      //form.submit();
-	  
-	$.ajax({
-		url: '/documents/' + this.value,
-		type: 'post',
-		data: {'_method': 'delete'},
-		success: function(d){
-			;
-		},
-		error: function(){
-			alert('error')
-		}
-	});
+    $.ajax({
+      url: '/documents/' + this.value,
+      type: 'post',
+      data: {'_method': 'delete'},
+      success: function(d){
+        ;
+      },
+      error: function(){
+        alert('error')
+      }
+    });
   }
 });
+(function comet(){
+  $.ajax({
+    url: '/comet',
+    type: 'json',
+    success: function(data){
+      
+      comet();
+    },
+    error: function(){
+      setTimeout(function(){comet()}, 3000);
+    }
+  });
+ })();
