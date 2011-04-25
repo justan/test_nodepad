96d7e0805337eb7abc97e46da77c5b99cf9dd142
justan <ckr189@163.com>
2011-04-25 22:33:16 +0800

    update model

--- models.js
+++ models.js
@@ -9,13 +9,24 @@ var Document = new Schema({
   user_id: {type: Number, index: true},
 }),
 User = new Schema({
-  email: {type: String, index: true, unique: true},
-  salt: {type: String, default: function(){
-    return Math.round((new Date().valueOf() * Math.random())) + '';
-  }}
+  email: {type: String, index: {unique: true}},
+  salt: String,
+  hashed_password: String
 });
 User.virtual('password').set(function(password){
-  this.set('_password', crypto.createHmac('sha1', this.salt).update(password).digest('hex'));
+  this.salt = this.makeSalt();
+  this._password = password;
+  this.hashed_password = this.encryptPassword(password);
+});
+
+User.method('encryptPassword', function(password){
+  return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+});
+User.method('makeSalt', function(){
+  return Math.round((new Date().valueOf() * Math.random())) + ''
+});
+User.method('authenticate', function(plainText){
+  return this.encryptPassword(plainText) === this.hashed_password;
 });
 
 mongoose.model('Document', Document);
